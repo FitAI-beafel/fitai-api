3bb51ba371f6b17294ee02957b0f77edd728b394
Alves <beatrizalvesfel@gmail.com>
2026-03-05 22:24:34 -0300

    feat: add sign in with google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,9 +7,6 @@ import { prisma } from "./db.js";
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
   trustedOrigins: ["http://localhost:3000"],
-  emailAndPassword: {
-    enabled: true,
-  },
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
